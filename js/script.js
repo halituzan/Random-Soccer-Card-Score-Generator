e26447a2b52e38236fab24ba0769c0ef36be8a81
Halit Uzan <halit.uzan@gmail.com>
2022-11-15 00:12:41 +0300

    add country flags

--- js/script.js
+++ js/script.js
@@ -12,16 +12,42 @@ let blah = document.getElementById("blah")
 let frameBlah = document.getElementById("frame-blah")
 let unlem = document.querySelector(".unlem")
 let uyari = document.querySelector(".uyari")
-
-
-
+let countries = document.querySelector("#countries")
+let countryFlag = document.querySelector("#countryflag")
+let defaultimage = document.querySelector("#defaultimage")
 let arr = []
+let countriesData = []
+
+let cardsValue = [
+    {
+        value: "black-gold",
+        image: "soccer-card-default.png"
+    },
+    {
+        value: "gold",
+        image: "soccer-card-gold.png"
+    },
+    {
+        value: "blue-gold",
+        image: "soccer-card-blue-gold.png"
+    },
+    {
+        value: "champions-league-black",
+        image: "champions-league-black.png"
+    },
+    {
+        value: "champions-league-blue",
+        image: "champions-league-blue.png"
+    },
+]
+
+
 formSub.addEventListener("submit", (e) => {
     arr = []
     e.preventDefault()
     const data = new FormData(e.target);
     const values = Object.fromEntries(data.entries())
-    const { maxNumber, minNumber, position } = values
+    const { maxNumber, minNumber, position, countries, cards } = values
     for (let i = 0; i < 6; i++) {
         arr.push(Math.floor(Math.random() * (maxNumber - minNumber)) + Number(minNumber))
         if (i === 5) {
@@ -37,14 +63,18 @@ formSub.addEventListener("submit", (e) => {
             totalScore.innerHTML = Math.floor(total / 6)
         }
     }
-    console.log(values.position)
 
+    let selectedCountry = countriesData.filter(i => i.alpha3 === countries)
+    countryFlag.innerHTML = `<img src="${selectedCountry[0].file_url}" alt="${selectedCountry[0].name}" style="width: 50px!important;height:30px!important;
+"/>`
+    let selectedCard = cardsValue?.filter(i => i.value === cards)
+    defaultimage.src = `./image/${selectedCard[0].image}`
     const [file] = imagePerson.files
     if (file) {
         frameBlah.src = URL.createObjectURL(file);
         frameBlah.style.display = "block";
     }
-    console.log(frameBlah)
+
 
 
 })
@@ -56,9 +86,18 @@ imagePerson.onchange = evt => {
         blah.style.display = "block"
     }
 }
-unlem.onmouseover = e =>{
-    uyari.style.display ="flex"
+unlem.onmouseover = e => {
+    uyari.style.display = "flex"
 }
-unlem.onmouseout = e =>{
-    uyari.style.display ="none"
+unlem.onmouseout = e => {
+    uyari.style.display = "none"
 }
+
+(async function () {
+    await fetch('./js/countries.json')
+        .then((response) => response.json())
+        .then((json) => countriesData = json);
+    countries.innerHTML += countriesData.map(item => `<option value="${item.alpha3}">${item.name}</option>`)
+
+}())
+
